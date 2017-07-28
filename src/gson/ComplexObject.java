87d457d1c9a45a82e2d8e7f65761cbd5cff30941
joshua924 <sz1288@nyu.edu>
2017-07-27 21:15:24 -0700

    update test case

--- src/gson/ComplexObject.java
+++ src/gson/ComplexObject.java
@@ -1,12 +1,15 @@
 package gson;
 
+import groovy.transform.builder.Builder;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.Map;
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -14,5 +17,34 @@ import java.util.Map;
 public class ComplexObject {
     private Duration duration;
     private LocalTime startTime;
-    private Map<ComplexObject, LocalTime> sons;
+    private Map<AgeRange, Set<Person>> persons;
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Builder
+    public static class Person {
+        private BigDecimal age;
+        private String name;
+        private Sex sex;
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class AgeRange {
+        private BigDecimal start;
+        private BigDecimal end;
+    }
+
+    public enum Sex {
+        MALE(0),
+        FEMALE(1);
+
+        private final int num;
+
+        Sex(int num) {
+            this.num = num;
+        }
+    }
 }
--- src/gson/SerializeAndDeserialize.java
+++ src/gson/SerializeAndDeserialize.java
@@ -1,12 +1,16 @@
 package gson;
 
+import com.google.common.collect.ImmutableSet;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
+import gson.ComplexObject.*;
 
 public class SerializeAndDeserialize {
     public static void main(String[] args) {
@@ -22,10 +26,18 @@ public class SerializeAndDeserialize {
         ComplexObject co = new ComplexObject();
         co.setDuration(Duration.ofHours(3));
         co.setStartTime(LocalTime.NOON);
-        Map<ComplexObject, LocalTime> sons = new HashMap<>();
-        ComplexObject son = new ComplexObject(Duration.ofMinutes(30), LocalTime.MIDNIGHT, null);
-        sons.put(son, LocalTime.MIDNIGHT);
-        co.setSons(sons);
+        co.setPersons(makePersons());
         return co;
+    }
+
+    private static Map<AgeRange, Set<Person>> makePersons() {
+        Map<AgeRange, Set<Person>> persons = new HashMap<>();
+        Person person1 = new Person(BigDecimal.valueOf(24.2), "Shuang Zhou", Sex.MALE);
+        Person person2 = new Person(BigDecimal.valueOf(26.5), "Qingqing Xiao", Sex.FEMALE);
+        AgeRange ageRange1 = new AgeRange(BigDecimal.ZERO, BigDecimal.valueOf(25));
+        AgeRange ageRange2 = new AgeRange(BigDecimal.valueOf(25), BigDecimal.valueOf(100));
+        persons.put(ageRange1, ImmutableSet.of(person1));
+        persons.put(ageRange2, ImmutableSet.of(person2));
+        return persons;
     }
 }

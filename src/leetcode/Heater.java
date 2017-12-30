d3eef1c1f098138a5fa9e145dbcc585cd3c4433c
joshua924 <sz1288@nyu.edu>
2017-12-30 15:19:28 -0800

    leetcode work.

---
+++ src/leetcode/Heater.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class Heater {
+    public int findRadius(int[] houses, int[] heaters) {
+        Arrays.sort(houses);
+        Arrays.sort(heaters);
+        int heaterIndex = 0;
+        int minRadius = Math.abs(houses[0] - heaters[0]);
+        for (int house : houses) {
+            while (heaterIndex < heaters.length && heaters[heaterIndex] < house) {
+                heaterIndex++;
+            }
+            if (heaterIndex == 0) {
+                minRadius = Math.max(minRadius, Math.abs(house - heaters[0]));
+            } else if (heaterIndex == heaters.length) {
+                minRadius = Math.max(minRadius, Math.abs(house - heaters[heaterIndex - 1]));
+            } else {
+                minRadius = Math.max(minRadius, Math.min(house - heaters[heaterIndex - 1], heaters[heaterIndex] - house));
+            }
+        }
+        return minRadius;
+    }
+
+    public static void main(String[] args) {
+        Heater h = new Heater();
+        int[] houses = {1, 2, 3, 4};
+        int[] heaters = {1, 4};
+        System.out.println(h.findRadius(houses, heaters));
+    }
+}
---
+++ src/leetcode/SlidingWindowMedian.java
@@ -0,0 +1,48 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class SlidingWindowMedian {
+    public double[] medianSlidingWindow(int[] nums, int k) {
+        List<Integer> current = new ArrayList<>();
+        double[] res = new double[nums.length - k + 1];
+        for (int i = 0; i < k; i++) {
+            int index = Collections.binarySearch(current, nums[i]);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            current.add(index, nums[i]);
+        }
+        res[0] = getMedian(current);
+        for (int i = k; i < nums.length; i++) {
+            int remove = Collections.binarySearch(current, nums[i - k]);
+            int index = Collections.binarySearch(current, nums[i]);
+            if (index < 0) {
+                index = -index - 1;
+            }
+            current.add(index, nums[i]);
+            res[i - k + 1] = getMedian(current);
+        }
+        return res;
+    }
+
+    private double getMedian(List<Integer> current) {
+        int size = current.size();
+        if (size % 2 == 0) {
+            return current.get(size / 2 - 1) / 2.0 + current.get(size / 2) / 2.0;
+        } else {
+            return (double) current.get(size / 2);
+        }
+    }
+
+    public static void main(String[] args) {
+        SlidingWindowMedian swm = new SlidingWindowMedian();
+        int[] nums = {2147483647, 2147483647};
+        double[] medians = swm.medianSlidingWindow(nums, 2);
+        for (double median : medians) {
+            System.out.print(median + " ");
+        }
+    }
+}

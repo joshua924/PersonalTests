079fe8f825d099832436de154060a084acbb738b
Joshua Zhou <zhoushua@amazon.com>
2018-02-04 15:14:40 -0800

    Add new changes

---
+++ src/lc/sz1288/UnsortedSubArray.java
@@ -0,0 +1,32 @@
+package lc.sz1288;
+
+public class UnsortedSubArray {
+    public int findUnsortedSubarray(int[] nums) {
+        int n = nums.length;
+        int start = -1;
+        int end = -2;
+        int min = nums[n - 1];
+        int max = nums[0];
+        for (int i = 1; i < n; i++) {
+            max = Math.max(max, nums[i]);
+            min = Math.min(min, nums[n - 1 - i]);
+            if (nums[i] < max) {
+                end = i;
+            }
+            if (nums[n - 1 - i] > min) {
+                start = n - 1 - i;
+            }
+        }
+        return end - start + 1;
+    }
+
+    public static void main(String[] args) {
+        UnsortedSubArray usa = new UnsortedSubArray();
+        int[] nums1 = {1, 2, 4, 5, 3};
+        int[] nums2 = {2, 6, 4, 8, 10, 9, 15};
+        int[] nums3 = {3, 3, 3, 4};
+        System.out.println(usa.findUnsortedSubarray(nums1));
+        System.out.println(usa.findUnsortedSubarray(nums2));
+        System.out.println(usa.findUnsortedSubarray(nums3));
+    }
+}

5da7add9cba00baae5095866c8da20feeb7f3e3f
Joshua Zhou <zhoushuang924@hotmail.com>
2021-03-12 16:10:49 -0800

    optimize imports

--- src/main/java/com/abnb/ImageTypeChecker.java
+++ src/main/java/com/abnb/ImageTypeChecker.java
@@ -1,17 +1,5 @@
 package com.abnb;
 
-import com.sun.xml.internal.ws.api.message.Header;
-import javafx.util.Pair;
-
-import javax.imageio.ImageIO;
-import javax.imageio.ImageReader;
-import javax.imageio.stream.FileImageInputStream;
-import javax.imageio.stream.ImageInputStream;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Iterator;
-
 /**
  * A runner to compare 2 image processing mechanisms: one that reads the image input stream header, another that
  * loads the whole image to get the dimensions. Currently the analysis show great difference between the 2,

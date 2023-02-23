f23249f3211531e0fdfe8599741148179b169a39
Joshua Zhou <zhoushuang924@hotmail.com>
2023-02-22 16:20:18 -0800

    regex parsing violation

---
+++ src/main/java/lc/sz1288/ValidatorRegex.java
@@ -0,0 +1,23 @@
+package lc.sz1288;
+
+
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ValidatorRegex {
+    public static void main(String[] args) {
+        String result = extractPath();
+        System.out.println(result);
+    }
+
+    private static String extractPath() {
+        String s = "[recursive_violation:request:[recursive_violation:damagedItemInfo:[non_null_violation:cost]]]";
+        return Stream.of(s.split(":\\[")).map(
+                each -> {
+                    String segment = each.replaceAll("[\\[|\\]]", "");
+                    String fieldName = segment.split(":")[1];
+                    return fieldName.replaceAll("([a-z])([A-Z]+)", "$1_$2").toLowerCase();
+                }
+        ).collect(Collectors.joining("."));
+    }
+}

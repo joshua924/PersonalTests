bf8e027d2a3db72ea6312b797301a27f4b2a31fc
Joshua Zhou <zhoushuang924@hotmail.com>
2023-03-31 19:28:26 -0700

    test completable future behavior

--- src/main/java/com/abnb/CompletableFutureTesting.java
+++ src/main/java/com/abnb/CompletableFutureTesting.java
@@ -1,16 +1,35 @@
 package com.abnb;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
 
 public class CompletableFutureTesting {
+
+    /**
+     * the exceptionally() method will not wrap or unwrap the exception.
+     */
+    public static void main(String[] args) {
+        CompletableFuture<Boolean> failedFuture = new CompletableFuture<>();
+        failedFuture.completeExceptionally(new CompletionException(new IllegalArgumentException("bad input")));
+
+        failedFuture.exceptionally(
+                e -> {
+                    System.out.println(e.getClass().getName());
+                    System.out.println(e instanceof IllegalArgumentException);
+                    System.out.println(e.getLocalizedMessage());
+                    return false;
+                }
+        ).join();
+    }
+
     /**
      * CompletableFuture.completedFuture() method can't pass exceptions along if they occur during the evaluation of
      * the parameter. Instead, the exception will be thrown outside of the completable future. In order for it to be a
      * completable future that finishes exceptionally, we need to use CompletableFuture.supplyAsync().
      */
-    public static void main(String[] args) {
+    private static void testExecution() {
         CompletableFuture<Boolean> policyCheck = CompletableFuture.supplyAsync(
-                CompletableFutureTesting::validate)
+                        CompletableFutureTesting::validate)
                 .thenCompose(ignored -> CompletableFuture.completedFuture(true));
         System.out.println(policyCheck.handle(
                 (response, exception) -> {

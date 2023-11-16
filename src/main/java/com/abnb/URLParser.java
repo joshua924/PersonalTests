969c8e4c870069926c6b95cc979e9d8cc765cc22
Joshua <zhoushuang924@hotmail.com>
2023-11-16 14:59:36 -0800

    Test URL parser

---
+++ src/main/java/com/abnb/URLParser.java
@@ -0,0 +1,18 @@
+package com.abnb;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class URLParser {
+    public static void main(String[] args) {
+        String s = "https://airbnb-photos.s3.amazonaws.com/pictures/check-in-guide/CheckInGuideStep-43388195/original/sdfsdcesdsd.png?a=c&c=d&e=f&Signature=3278rgsdehkfds";
+        try {
+            URL url = new URL(s);
+            System.out.println(url.getHost());
+            System.out.println(url.getPath());
+            System.out.println(url.getQuery());
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("Invalid URL " + s);
+        }
+    }
+}
